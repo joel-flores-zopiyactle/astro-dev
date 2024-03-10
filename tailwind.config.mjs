67c26c28b8bef2668ccaaf58f92181ee8413f1a6
Joel <joefloreszopiyactle@gmail.com>
2024-03-09 20:40:17 -0600

    Addd icons animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+			},  
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+			},
+			keyframes: {
+				'infinite-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' },
+				}
+			}  
+		},
 	},
 	plugins: [],
 }
